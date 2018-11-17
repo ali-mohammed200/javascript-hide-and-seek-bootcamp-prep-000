06218482dfa5b31e2f1b491461e8d5002eb8f8c9
ali.mohammed200 <ali.mohammed200@gmail.com>
2018-11-17 23:07:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+function getFirstSelector(selector){
+  let selected = document.querySelector(selector);
+  return selected;
+}
 
+function nestedTarget(){
+  let selected = document.querySelector('#nested .target');
+  return selected;
+}
+
+function increaseRankBy(n){
+  let lis = document.querySelectorAll('ul.ranked-list li');
+  for(let i = 0; i < lis.length; i++){
+    lis[i].innerHTML = +lis[i].innerHTML + n;
+  }
+}
+
+function deepestChild(){
+  let selected = document.querySelector('#grand-node div div div div');
+  return selected;
+}
